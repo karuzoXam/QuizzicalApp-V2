37548956356d248ad4227bcbcc7f71a48460482e
Max <60605508+karuzoXam@users.noreply.github.com>
2024-01-31 12:00:10 +0100

    added more specific error messages

--- QuizzicalV2/src/fetchData.js
+++ QuizzicalV2/src/fetchData.js
@@ -2,7 +2,12 @@ async function fetchData() {
   try {
     const res = await fetch('https://opentdb.com/api.php?amount=5');
     if (!res.ok) {
-      throw new Error(`Failed to fetch! Error: ${res.status}  `);
+      const errorMessage = `Failed to fetch! Error:(${res.status})`;
+      if (res.status === 429) {
+        throw new Error(errorMessage + ` Too Many Requests`);
+      } else {
+        throw new Error(errorMessage);
+      }
     }
     const data = await res.json();
     return data;
