19d561600f12c176ae0704dbdb47fb6a5b35ffe2
Max <60605508+karuzoXam@users.noreply.github.com>
2024-02-03 15:19:50 +0100

    added background img

--- QuizzicalV2/src/Components/Start.jsx
+++ QuizzicalV2/src/Components/Start.jsx
@@ -2,7 +2,7 @@ function Start(props) {
   return (
     <div>
       <h1 className="start_header">Quizzical</h1>
-      <p className="start_description">Some description if needed</p>
+      <p className="start_description">Click Start quiz to start the game!</p>
       <button className="start_btn" onClick={props.handleStartClick}>
         Start quiz
       </button>
